87bce2759a90c829a0ae3eb612ad383f46a160c3
caracer1 <61810767+caracer1@users.noreply.github.com>
2021-03-09 16:47:23 -0800

    Update user_bottle.py

--- user_bottle.py
+++ user_bottle.py
@@ -4,31 +4,30 @@ import json
 from bottle import request, response
 from bottle import post, get, put, delete
 import bottle
-from bottle.ext import sqlite
-
 
 
 @post('/users/')
 def user_creation():
     '''Handles name creation'''
-    try:
-        # parse input data
-        try:
-            data = request.json()
-        except:
-            raise ValueError
-        if data is None:
-            raise ValueError
-        username=data['username']  
-        password=data['password']
-        email=data['email']
-        response.status = "200 OK"
-    except:
-        #response.headers['Content-Type'] = 'application/json'
-        #print("hi")
-        response.status = "400 invalid"
-    finally:
-        return response.status  
+##    try:
+##        # parse input data
+##        try:
+##            data = request.json()
+##        except:
+##            raise ValueError
+##        if data is None:
+##            raise ValueError
+##        username=data['username']  
+##        password=data['password']
+##        email=data['email']
+##        response.status = "200 OK"
+##    except:
+##        #response.headers['Content-Type'] = 'application/json'
+##        #print("hi")
+##        response.status = "400 invalid"
+##    finally:
+    test = "Hello, world"
+    return test   
 
 @get('/users/checkPassword/<username>/<password>/')
 def checkPassword(username,password):
@@ -44,51 +43,53 @@ def checkPassword(username,password):
     return response.status
 
 
-@put('/users/follower')
+@put('/users/addfollower')
 def addFollower():
-    try:
-        # parse input data
-        try:
-            data = request.json()
-        except:
-            raise ValueError
-        if data is None:
-            raise ValueError
-        username=data['username']
-        usernameToFollow=data['usernametofollow']
-        response.status = "200 OK"
+##    try:
+##        # parse input data
+##        try:
+##            data = request.json()
+##        except:
+##            raise ValueError
+##        if data is None:
+##            raise ValueError
+##        username=data['username']
+##        usernameToFollow=data['usernametofollow']
+#Í#        response.status = "200 OK"
+##
+##
+##    except:
+##        response.status = "400 INVALID"
+##
 
-
-    except:
-        response.status = "400 INVALID"
-
-
-    return response.status
+    test = "Hello, world"
+    return test  
 
     
        
-@delete('/users/follower/<username>')
+@delete('/users/removefollower/<username>/')
 def deletefollower():
-    try:
-        # parse input data
-        try:
-            data = request.json()
-        except:
-            raise ValueError
-        if data is None:
-            raise ValueError
-        usernameToFollow=data['usernametounfollow']
-        response.status = 200
-    except:
-        response.status = 400
+##    try:
+##        # parse input data
+##        try:
+##            data = request.json()
+##        except:
+##            raise ValueError
+##        if data is None:
+##            raise ValueError
+##        usernameToFollow=data['usernametounfollow']
+##        response.status = 200
+##    except:
+##        response.status = 400
+    test = "Hello, world"
+    return test 
 
-    return response.status 
 
-
-@get('/users/<username>/follower')
+@get('/users/<username>/')
 def getFollowers(username):
     
-    return sql_data
+    test = "Hello, world"
+    return test 
 
 if __name__ == '__main__':
     bottle.run(host = '127.0.0.1', port = 8000) 
